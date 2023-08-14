fdd90930f60c1b05d96a9d3abc8a08e75614cdda
wilverine7 <95718700+wilverine7@users.noreply.github.com>
2023-08-14 11:48:30 -0600

    fixing env varibales to have ""

--- app.py
+++ app.py
@@ -178,7 +178,7 @@ def ListingUpload():
         # this is the category to attribute set sheet
         # open Attribute Set Attribute sheet and convert it to a DF
         attributeUrl = "https://docs.google.com/spreadsheets/d/1tXm039Fcj16Qn1rWd6HzpyoqQ_l0H64tlmV_0nVDmIk/edit#gid=626682809"
-        sa = gspread.service_account_from_dict(os.getenv(gsheetskey))
+        sa = gspread.service_account_from_dict(os.getenv("gsheetskey"))
         sh = sa.open_by_url(attributeUrl)
         ws = sh.worksheet("PrimaryToAttributeSet")
         primaryToAttributeDf = pd.DataFrame(ws.get_all_records())
@@ -390,9 +390,9 @@ def UrlUpload():
 
         else:
             # connect to server
-            hostname = os.getenv(hostname)
-            username = os.getenv(username)
-            password = os.getenv(password)
+            hostname = os.getenv("hostname")
+            username = os.getenv("username")
+            password = os.getenv("password")
 
             cnopts = pysftp.CnOpts()
             cnopts.hostkeys = None
@@ -447,9 +447,9 @@ def UrlUpload():
 
         else:
             # connect to server
-            hostname = os.getenv(hostname)
-            username = os.getenv(username)
-            password = os.getenv(password)
+            hostname = os.getenv("hostname")
+            username = os.getenv("username")
+            password = os.getenv("password")
 
             cnopts = pysftp.CnOpts()
             cnopts.hostkeys = None
@@ -534,9 +534,9 @@ def ImageCsvTest():
             # doesn't require the export sheet. You can export the sourcing sheet
             # CaDf = pd.DataFrame(columns=["Inventory Number", "Picture URLs"])
             uniqueParentColor = df["Parent SKU_Color"].unique()
-            hostname = os.getenv(hostname)
-            username = os.getenv(username)
-            password = os.getenv(password)
+            hostname = os.getenv("hostname")
+            username = os.getenv("username")
+            password = os.getenv("password")
             columns = []
             cnopts = pysftp.CnOpts()
             cnopts.hostkeys = None
@@ -975,9 +975,9 @@ def DeleteImage():
     print(file)
 
     server_path = f"public_html/media{file}"
-    hostname = os.getenv(hostname)
-    username = os.getenv(username)
-    password = os.getenv(password)
+    hostname = os.getenv("hostname")
+    username = os.getenv("username")
+    password = os.getenv("password")
 
     cnopts = pysftp.CnOpts()
     cnopts.hostkeys = None
@@ -1001,9 +1001,9 @@ def DeleteSingleImage():
     folder_name = datetime.today().strftime("%Y-%m-%d")
 
     server_path = f"public_html/media/L9/{folder_name}/{sku}_{imageNumber}.jpg"
-    hostname = os.getenv(hostname)
-    username = os.getenv(username)
-    password = os.getenv(password)
+    hostname = os.getenv("hostname")
+    username = os.getenv("username")
+    password = os.getenv("password")
 
     cnopts = pysftp.CnOpts()
     cnopts.hostkeys = None
