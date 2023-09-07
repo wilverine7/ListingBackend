09071851ca2ad38583582eebb8f91b6d76bfff70
wilverine7 <95718700+wilverine7@users.noreply.github.com>
2023-09-07 13:00:54 -0600

    update logger

--- functions.py
+++ functions.py
@@ -15,95 +15,96 @@ def removeBackground(url, imageName):
         hostname = app.config["HOSTNAME"]
         username = app.config["USERNAME"]
         password = app.config["PASSWORD"]
-
-        folder_name = datetime.today().strftime("%Y-%m-%d")
-        cnopts = pysftp.CnOpts()
-        cnopts.hostkeys = None
-
-        server_path = f"public_html/media/L9/{folder_name}/{imageName}.jpg"
-        try:
-            with pysftp.Connection(
-                hostname, username=username, password=password, cnopts=cnopts
-            ) as sftp:
-                with sftp.cd("public_html/media/L9/"):
-                    if sftp.exists(folder_name):
-                        pass
-                    else:
-                        # create new directory at public_html/media/L9/ with the folder_name variable
-                        sftp.mkdir(folder_name)
-                try:
-                    # open the image from the url
-                    response = requests.get(image_url, stream=True)
-                    # convert the image to a PIL image
-                    image = Image.open(BytesIO(response.content))
-
-                    # remove background from image
-                    image = rembg.remove(image)
-
-                    # creates a file like object to save the image to
-                    image_io = BytesIO()
-                    # saves the image with no background as a png
-                    image.save(image_io, format="PNG")
-
-                    # open it again with PIL so we can process it
-                    image2 = Image.open(image_io)
-                    h, w = image2.size
-                    if h > w:
-                        imageSize = (h, h)
-                    else:
-                        imageSize = (w, w)
-
-                    # Create a new square image with a white background
-                    square_image = Image.new("RGBA", (imageSize), (255, 255, 255, 0))
-
-                    # Calculate the position to center the original image on the new canvas
-                    position = (
-                        (square_image.width - image2.width) // 2,
-                        (square_image.height - image2.height) // 2,
-                    )
-
-                    # Paste the original image into the center of the square image
-                    square_image.paste(image2, position, image2)
-
-                    # convert the image to RGB
-                    square_image = square_image.convert("RGB")
-
-                    square_image = square_image.resize((1200, 1200))
-
-                    image_io2 = BytesIO()
-                    # Save the resized image to a file-like object
-                    square_image.save(image_io2, format="JPEG")
-
-                    # Upload the image to the server
-                    image_io2.seek(0)  # Reset the file pointer to the beginning
-                    sftp.putfo(image_io2, server_path)
-
-                    # close connection
-                    sftp.close()
-
-                    # creates a variable to pass to the html page to display the image and url
-                    BikeWagonUrl = (
-                        f"https://bikewagonmedia.com/media/L9/{folder_name}/{imageName}.jpg"
-                    )
-                    app.logger.info("Background removed successfully")
-                    return BikeWagonUrl
-                except Exception as e:
-                    print(f"Error: {str(e)}")
-                    error = "There was an error processing the image"
-                    json_error = {"error": error}
-                    app.logger.error("Error processing image")
-                    return json_error
-        except Exception as e:
-            print(f"Error: {str(e)}")
-            error = "There was an error connecting to the server in remove background"
-            json_error = {"error": error}
-            app.logger.error("Error connecting to server in remove background")
-            return json_error
     except Exception as e:
         print(f"Error: {str(e)}")
-        error = "There was an error with the request remove background"
+        error = "There was an error getting the configuration values"
         json_error = {"error": error}
         return json_error
+
+    folder_name = datetime.today().strftime("%Y-%m-%d")
+    cnopts = pysftp.CnOpts()
+    cnopts.hostkeys = None
+
+    server_path = f"public_html/media/L9/{folder_name}/{imageName}.jpg"
+    try:
+        with pysftp.Connection(
+            hostname, username=username, password=password, cnopts=cnopts
+        ) as sftp:
+            with sftp.cd("public_html/media/L9/"):
+                if sftp.exists(folder_name):
+                    pass
+                else:
+                    # create new directory at public_html/media/L9/ with the folder_name variable
+                    sftp.mkdir(folder_name)
+            try:
+                # open the image from the url
+                response = requests.get(image_url, stream=True)
+                # convert the image to a PIL image
+                image = Image.open(BytesIO(response.content))
+
+                # remove background from image
+                image = rembg.remove(image)
+
+                # creates a file like object to save the image to
+                image_io = BytesIO()
+                # saves the image with no background as a png
+                image.save(image_io, format="PNG")
+
+                # open it again with PIL so we can process it
+                image2 = Image.open(image_io)
+                h, w = image2.size
+                if h > w:
+                    imageSize = (h, h)
+                else:
+                    imageSize = (w, w)
+
+                # Create a new square image with a white background
+                square_image = Image.new("RGBA", (imageSize), (255, 255, 255, 0))
+
+                # Calculate the position to center the original image on the new canvas
+                position = (
+                    (square_image.width - image2.width) // 2,
+                    (square_image.height - image2.height) // 2,
+                )
+
+                # Paste the original image into the center of the square image
+                square_image.paste(image2, position, image2)
+
+                # convert the image to RGB
+                square_image = square_image.convert("RGB")
+
+                square_image = square_image.resize((1200, 1200))
+
+                image_io2 = BytesIO()
+                # Save the resized image to a file-like object
+                square_image.save(image_io2, format="JPEG")
+
+                # Upload the image to the server
+                image_io2.seek(0)  # Reset the file pointer to the beginning
+                sftp.putfo(image_io2, server_path)
+
+                # close connection
+                sftp.close()
+
+                # creates a variable to pass to the html page to display the image and url
+                BikeWagonUrl = (
+                    f"https://bikewagonmedia.com/media/L9/{folder_name}/{imageName}.jpg"
+                )
+                app.logger.info("Background removed successfully")
+                return BikeWagonUrl
+            except Exception as e:
+                print(f"Error: {str(e)}")
+                error = "There was an error processing the image"
+                json_error = {"error": error}
+                app.logger.error("Error processing image")
+                return json_error
+    except Exception as e:
+        print(f"Error: {str(e)}")
+        error = "There was an error connecting to the server in remove background"
+        json_error = {"error": error}
+        app.logger.error("Error connecting to server in remove background")
+        return json_error
+    
 
 def process_image(image):
     from PIL import Image
