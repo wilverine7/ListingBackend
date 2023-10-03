afe459de6f4542e2c9600393b75d12984c3865b7
wilverine7 <95718700+wilverine7@users.noreply.github.com>
2023-10-03 13:11:26 -0600

    fixed gunicorn error

--- app.py
+++ app.py
@@ -17,7 +17,6 @@ from openpyxl.worksheet.datavalidation import DataValidation
 import gspread
 import logging
 import sys
-from gunicorn.errors import WorkerTimeout
 
 
 pd.options.mode.chained_assignment = None  # default='warn'
@@ -63,13 +62,6 @@ def handle_exception(exc_type, exc_value, exc_traceback):
     app.logger.error("Uncaught exception", exc_info=(exc_type, exc_value, exc_traceback))
 
 sys.excepthook = handle_exception
-
-@app.errorhandler(WorkerTimeout)
-def handle_timeout_error(e):
-    json = {"error": "Request timed out. Please try again later.",
-            "status": 504
-            }
-    return jsonify(json), 504
 
 @app.route("/", methods=["GET"])
 def index():
