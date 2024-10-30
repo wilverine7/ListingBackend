8122c551bfd4e7dfc35949e6d1932771740bc6e8
wilverine7 <95718700+wilverine7@users.noreply.github.com>
2024-10-30 15:36:03 -0600

    logs

--- app.py
+++ app.py
@@ -1272,7 +1272,8 @@ def getImageCsv():
 @app.route("/ImageCsv", methods=["GET", "POST"])
 @cross_origin(supports_credentials=True)
 def start_task():
-    app.logger.info(app.config["redis_password"])
+    app.logger.info(f"redis password: {app.config['redis_password']}")
+    app.logger.info(f"hostname: {app.config["HOSTNAME"]}")
     task_id = str(uuid.uuid4())
     task_data = {"progress": 0, "chunks": "0"}
     redis_client.set(task_id, json.dumps(task_data))
